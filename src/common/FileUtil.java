cf4cf8b77b169515879ccb805091beae611ef712
= <=>
2021-10-05 22:17:32 +0700

    commit

--- src/common/FileUtil.java
+++ src/common/FileUtil.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.io.NotSerializableException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 /**
@@ -37,6 +38,14 @@ public class FileUtil {
             System.out.println("The Object  was succesfully written to a file");
 
         } catch (Exception ex) {
+//            COMMENTATOR: ĐINH TIẾN LÂM(HE153097)
+//            Solution proposed on Sonarqube:
+//            
+//            try {
+//                /* ... */
+//            } catch (Exception e) {
+//                LOGGER.log("context", e); // Compliant
+//            }
             ex.printStackTrace();
         }
     }
@@ -44,6 +53,11 @@ public class FileUtil {
     public void writeObjects(ArrayList<Object> objectList) {
         try {
             FileOutputStream fos = new FileOutputStream(filePath, true);
+//    COMMENTATOR: ĐINH TIẾN LÂM(HE153097)
+//    Solution proposed on Sonarqube:
+//              
+//            FileOutputStream fos = new FileOutputStream(fileName);
+//            ObjectOutputStream out = new ObjectOutputStream(fos);
             try {
                 ObjectOutputStream oos = new ObjectOutputStream(fos);
                 for (Object anObject : objectList) {
@@ -52,18 +66,70 @@ public class FileUtil {
                         System.out.println("saved");
                     } catch (NotSerializableException e) {
                         System.out.println("An object was not serializable, it has not been saved.");
+                        //            COMMENTATOR: ĐINH TIẾN LÂM(HE153097)
+//            Solution proposed on Sonarqube:
+//            
+//            try {
+//                /* ... */
+//            } catch (Exception e) {
+//                LOGGER.log("context", e); // Compliant
+//            }
                         e.printStackTrace();
                     }
                 }
             } catch (IOException e) {
+                //            COMMENTATOR: ĐINH TIẾN LÂM(HE153097)
+//            Solution proposed on Sonarqube:
+//            
+//            try {
+//                /* ... */
+//            } catch (Exception e) {
+//                LOGGER.log("context", e); // Compliant
+//            }
                 e.printStackTrace();
             }
         } catch (FileNotFoundException e) {
+            //            COMMENTATOR: ĐINH TIẾN LÂM(HE153097)
+//            Solution proposed on Sonarqube:
+//            
+//            try {
+//                /* ... */
+//            } catch (Exception e) {
+//                LOGGER.log("context", e); // Compliant
+//            }
             e.printStackTrace();
         }
 
     }
 
+//    COMMENTATOR: ĐINH TIẾN LÂM(HE153097)
+//    Solution proposed on Sonarqube:
+//   
+//    private void readTheFile(String fileName) throws IOException {
+//        Path path = Paths.get(fileName);
+//        try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+//            reader.readLine();
+//            // ...
+//        }
+//        // ..
+//        try (Stream<String> input = Files.lines("input.txt")) {
+//            input.forEach(System.out::println);
+//        }
+//    }
+//
+//    private void doSomething() {
+//        OutputStream stream = null;
+//        try {
+//            stream = new FileOutputStream("myfile.txt");
+//            for (String property : propertyList) {
+//                // ...
+//            }
+//        } catch (Exception e) {
+//            // ...
+//        } finally {
+//            stream.close();
+//        }
+//    }
     public static ArrayList<Object> readObjects() {
         ArrayList<Object> objectList = new ArrayList<Object>();
         boolean cont = true;
@@ -74,6 +140,14 @@ public class FileUtil {
                 try {
                     obj = ois.readObject();
                 } catch (ClassNotFoundException e) {
+//            COMMENTATOR: ĐINH TIẾN LÂM(HE153097)
+//            Solution proposed on Sonarqube:
+//            
+//            try {
+//                /* ... */
+//            } catch (Exception e) {
+//                LOGGER.log("context", e); // Compliant
+//            }
                     e.printStackTrace();
                 }
                 if (obj != null) {
@@ -84,9 +158,25 @@ public class FileUtil {
             }
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
+//            COMMENTATOR: ĐINH TIẾN LÂM(HE153097)
+//            Solution proposed on Sonarqube:
+//            
+//            try {
+//                /* ... */
+//            } catch (Exception e) {
+//                LOGGER.log("context", e); // Compliant
+//            }
             e.printStackTrace();
         } catch (IOException e) {
             // TODO Auto-generated catch block
+//            COMMENTATOR: ĐINH TIẾN LÂM(HE153097)
+//            Solution proposed on Sonarqube:
+//            
+//            try {
+//                /* ... */
+//            } catch (Exception e) {
+//                LOGGER.log("context", e); // Compliant
+//            }
             e.printStackTrace();
         }
 
